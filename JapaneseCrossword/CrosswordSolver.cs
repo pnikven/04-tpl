64314416318a8772d5a53fe7d94611add082066b
pnikven <pyshkinnv@gmail.com>
2015-03-08 20:30:08 +0500

    прошел тест IncorrectOutputFile()

--- JapaneseCrossword/CrosswordSolver.cs
+++ JapaneseCrossword/CrosswordSolver.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace JapaneseCrossword
 {
-    public class CrosswordSolver : ICrosswordSolver
-    {
-        public SolutionStatus Solve(string inputFilePath, string outputFilePath)
-        {
-            if(!File.Exists(inputFilePath))
+	public class CrosswordSolver : ICrosswordSolver
+	{
+		public SolutionStatus Solve(string inputFilePath, string outputFilePath)
+		{
+			if (!File.Exists(inputFilePath))
 				return SolutionStatus.BadInputFilePath;
 
+			if(Path.GetInvalidFileNameChars().Any(outputFilePath.Contains))
+				return SolutionStatus.BadOutputFilePath;
+
 			throw new NotImplementedException();
-        }
-    }
+		}
+	}
 }
