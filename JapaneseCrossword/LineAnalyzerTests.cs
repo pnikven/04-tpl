d73a04da56a19d33f499bf3d4bedfbe3a0194493
pnikven <pyshkinnv@gmail.com>
2015-03-10 13:49:16 +0500

    тест для двух блоков в линии

--- JapaneseCrossword/LineAnalyzerTests.cs
+++ JapaneseCrossword/LineAnalyzerTests.cs
@@ -26,5 +26,19 @@ namespace JapaneseCrossword
 
 			Assert.AreEqual(expected, result);
 		}
+
+		[Test]
+		public void Analyze_TwoBlocks_ReturnsCorrectResult()
+		{
+			var line = new Line(LineType.Row, 1, new[] { 1, 2 });
+			var cells = new CellState[5];
+			var canBeFilled = new[] { true, true, true, true, true };
+			var canBeEmpty = new[] { true, true, true, false, true };
+			var expected = new LineAnalysisResult(canBeFilled, canBeEmpty);
+
+			var result = lineAnalyzer.Analyze(line, cells);
+
+			Assert.AreEqual(expected, result);
+		}
 	}
 }
