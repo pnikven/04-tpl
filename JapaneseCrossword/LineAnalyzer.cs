7b72eee7b10807486d7038483c2f30cbd0528662
pnikven <pyshkinnv@gmail.com>
2015-03-10 21:14:09 +0500

    прошел тест с двумя блоками (реализовал основную логику LineAnalyzer)

--- JapaneseCrossword/LineAnalyzer.cs
+++ JapaneseCrossword/LineAnalyzer.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using MoreLinq;
 
@@ -8,36 +5,85 @@ namespace JapaneseCrossword
 {
 	class LineAnalyzer : ILineAnalyzer
 	{
+		private const int MinSpaceBetweenBlocks = 1;
+
 		public ILineAnalysisResult Analyze(ILine line, CellState[] cells)
 		{
 			var canBeFilled = new bool[cells.Length];
 			var canBeEmpty = new bool[cells.Length];
 
 			var block = line.Blocks.First();
-			Enumerable.Range(0, cells.Length - block.Length + 1)
-				.Where(startPosition => TryBlock(block, startPosition, cells))
-				.Select(startPosition => Enumerable.Range(startPosition, block.Length).ToArray())
-				.ForEach(occupiedCells => UpdateFilledEmpty(canBeFilled, canBeEmpty, occupiedCells));
+			Enumerable.Range(0, GetMaxStartPositionOfBlock(block, line, cells.Length) + 1)
+				.Where(startPosition => TryBlock(block, startPosition, line, cells, canBeEmpty, canBeFilled))
+				.ForEach(startPosition => UpdateEmptyBeforeBlock(startPosition, canBeEmpty));
 			return new LineAnalysisResult(canBeFilled, canBeEmpty);
 		}
 
-		private static void UpdateFilledEmpty(bool[] canBeFilled, bool[] canBeEmpty, int[] occupiedCells)
+		private int GetMaxStartPositionOfBlock(IBlock block, ILine line, int cellsCount)
 		{
-			occupiedCells.ForEach(i => canBeFilled[i] = true);
-
-			Enumerable.Range(0, canBeEmpty.Length)
-				.Except(occupiedCells)
-				.ForEach(i => canBeEmpty[i] = true);
+			return cellsCount - line.Blocks.Skip(block.Index).Sum(b => b.Length + MinSpaceBetweenBlocks) + 1;
 		}
 
-		private bool TryBlock(IBlock block, int startPosition, CellState[] cells)
+		private void UpdateEmptyBeforeBlock(int blockStartPosition, bool[] canBeEmpty)
+		{
+			Enumerable.Range(0, blockStartPosition).ForEach(i => canBeEmpty[i] = true);
+		}
+
+		private bool TryBlock(IBlock block, int start, ILine line, CellState[] cells, bool[] canBeEmpty, bool[] canBeFilled)
 		{
 			if (cells
-				.Skip(startPosition)
+				.Skip(start)
 				.Take(block.Length)
 				.Any(cellState => cellState == CellState.Filled))
 				return false;
-			return true;
+
+			if (IsLastBlockInLine(block, line))
+			{
+				if (cells
+					.Skip(start + block.Length)
+					.Any(state => state == CellState.Filled))
+					return false;
+				UpdateFilledOnBlock(block, start, canBeFilled);
+				UpdateEmptyAfterBlock(block, start, canBeEmpty);
+				return true;
+			}
+
+			var result = false;
+			var nextBlock = line.Blocks.ElementAt(block.Index + 1);
+			var startPositionOfNextBlock = start + block.Length + MinSpaceBetweenBlocks;
+			Enumerable.Range(startPositionOfNextBlock,
+				GetMaxStartPositionOfBlock(nextBlock, line, cells.Length) - startPositionOfNextBlock + 1)
+				.Where(nextStart =>
+					cells[nextStart - 1] != CellState.Filled &&
+					TryBlock(nextBlock, nextStart, line, cells, canBeEmpty, canBeFilled))
+				.ForEach(nextStart =>
+				{
+					result = true;
+					UpdateFilledOnBlock(block, start, canBeFilled);
+					UpdateEmptyAfterBlockBeforeNextBlock(block, start, nextStart, canBeEmpty);
+				});
+
+			return result;
+		}
+
+		private void UpdateEmptyAfterBlockBeforeNextBlock(IBlock block, int start, int startNext, bool[] canBeEmpty)
+		{
+			for (var i = start + block.Length; i < startNext; i++) canBeEmpty[i] = true;
+		}
+
+		private void UpdateFilledOnBlock(IBlock block, int startPosition, bool[] canBeFilled)
+		{
+			Enumerable.Range(startPosition, block.Length).ForEach(i => canBeFilled[i] = true);
+		}
+
+		private void UpdateEmptyAfterBlock(IBlock block, int startPosition, bool[] canBeEmpty)
+		{
+			for (var i = startPosition + block.Length; i < canBeEmpty.Length; i++) canBeEmpty[i] = true;
+		}
+
+		private bool IsLastBlockInLine(IBlock block, ILine line)
+		{
+			return block.Index == line.BlockCount - 1;
 		}
 	}
 }
