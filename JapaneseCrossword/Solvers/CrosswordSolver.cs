b05897ca0f4ea289640a2fe9b829db15efb46caf
pnikven <pyshkinnv@gmail.com>
2015-03-15 16:41:56 +0500

    добавил еще один большой кроссворд в тестирование

--- JapaneseCrossword/Solvers/CrosswordSolver.cs
+++ JapaneseCrossword/Solvers/CrosswordSolver.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 using JapaneseCrossword.Enums;
@@ -56,7 +57,7 @@ namespace JapaneseCrossword.Solvers
 				contents = File.ReadAllText(filePath, Encoding.UTF8);
 				return true;
 			}
-			catch
+			catch (Exception e)
 			{
 				contents = null;
 				return false;
