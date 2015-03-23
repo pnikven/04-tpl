b47fc83e53f46d1eef169efa68b4a96d731bf689
pnikven <pyshkinnv@gmail.com>
2015-03-23 20:16:21 +0500

    добавил в Balancer ссылку на проект HashServer с Listener-ами

--- Balancer/Program.cs
+++ Balancer/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HashServer;
 
 namespace Balancer
 {
@@ -10,6 +6,7 @@ namespace Balancer
 	{
 		static void Main(string[] args)
 		{
+			Listener l=new Listener(1,"", null);
 		}
 	}
 }
