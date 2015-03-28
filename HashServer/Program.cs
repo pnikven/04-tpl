d28054ee357c33e821e7d5b8680f09a506c314ac
hexandr <alexandr.v.golubev@gmail.com>
2015-03-28 16:37:47 +0500

    порт для сервера можно передать при старте

--- HashServer/Program.cs
+++ HashServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Net;
+﻿using System.Linq;
+﻿using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
@@ -9,85 +10,88 @@ using log4net.Config;
 
 namespace HashServer
 {
-	class Program
-	{
-		private static int delayMs = 1000;
+    class Program
+    {
+        private static int delayMs = 1000;
+        private static int port = 20000;
 
-		static void Main(string[] args)
-		{
-			XmlConfigurator.Configure();
-			try
-			{
-				var listener = new Listener(port, "method", OnContextAsync);
-				listener.Start();
+        static void Main(string[] args)
+        {
+            if (args.Any())
+                int.TryParse(args[0], out port);
 
-				var listenerSync = new ListenerSync(port, "methodSync", OnContext);
-				listenerSync.Start();
+            XmlConfigurator.Configure();
+            try
+            {
+                var listener = new Listener(port, "method", OnContextAsync);
+                listener.Start();
 
-				log.InfoFormat("Server started!");
-				Console.WriteLine("Enter server delay in ms");
-				while (true)
-				{
-					var timeToSleepString = Console.ReadLine();
-					int timeToSleep;
-					if (int.TryParse(timeToSleepString, out timeToSleep))
-						delayMs = timeToSleep;
-					else
-						Console.WriteLine("Couldn't parse \"{0}\" as valid int.", timeToSleepString);
-					Console.WriteLine("Delay is {0} ms", delayMs);
-				}
-			}
-			catch(Exception e)
-			{
-				log.Fatal(e);
-				throw;
-			}
-		}
+                var listenerSync = new ListenerSync(port, "methodSync", OnContext);
+                listenerSync.Start();
 
-		private static async Task OnContextAsync(HttpListenerContext context)
-		{
-			var requestId = Guid.NewGuid();
-			var query = context.Request.QueryString["query"];
-			var remoteEndPoint = context.Request.RemoteEndPoint;
-			log.InfoFormat("{0}: received {1} from {2}", requestId, query, remoteEndPoint);
-			context.Request.InputStream.Close();
+                log.InfoFormat("Server started listening on port {0}!", port);
+                Console.WriteLine("Enter server delay in ms");
+                while (true)
+                {
+                    var timeToSleepString = Console.ReadLine();
+                    int timeToSleep;
+                    if (int.TryParse(timeToSleepString, out timeToSleep))
+                        delayMs = timeToSleep;
+                    else
+                        Console.WriteLine("Couldn't parse \"{0}\" as valid int.", timeToSleepString);
+                    log.InfoFormat("Delay is {0} ms", delayMs);
+                }
+            }
+            catch (Exception e)
+            {
+                log.Fatal(e);
+                throw;
+            }
+        }
 
-			await Task.Delay(delayMs);
+        private static async Task OnContextAsync(HttpListenerContext context)
+        {
+            var requestId = Guid.NewGuid();
+            var query = context.Request.QueryString["query"];
+            var remoteEndPoint = context.Request.RemoteEndPoint;
+            log.DebugFormat("{0}: received {1} from {2}", requestId, query, remoteEndPoint);
+            context.Request.InputStream.Close();
 
-			var hash = Convert.ToBase64String(CalcHash(Encoding.UTF8.GetBytes(query)));
-			var encryptedBytes = Encoding.UTF8.GetBytes(hash);
+            await Task.Delay(delayMs);
 
-			await context.Response.OutputStream.WriteAsync(encryptedBytes, 0, encryptedBytes.Length);
-			context.Response.OutputStream.Close();
-			log.InfoFormat("{0}: {1} sent back to {2}", requestId, hash, remoteEndPoint);
-		}
+            var hash = Convert.ToBase64String(CalcHash(Encoding.UTF8.GetBytes(query)));
+            var encryptedBytes = Encoding.UTF8.GetBytes(hash);
 
-		private static void OnContext(HttpListenerContext context)
-		{
-			var requestId = Guid.NewGuid();
-			var query = context.Request.QueryString["query"];
-			var remoteEndPoint = context.Request.RemoteEndPoint;
-			log.InfoFormat("{0}: received {1} from {2}", requestId, query, remoteEndPoint);
-			context.Request.InputStream.Close();
+            await context.Response.OutputStream.WriteAsync(encryptedBytes, 0, encryptedBytes.Length);
+            context.Response.OutputStream.Close();
+            log.DebugFormat("{0}: {1} sent back to {2}", requestId, hash, remoteEndPoint);
+        }
 
-			Thread.Sleep(delayMs);
+        private static void OnContext(HttpListenerContext context)
+        {
+            var requestId = Guid.NewGuid();
+            var query = context.Request.QueryString["query"];
+            var remoteEndPoint = context.Request.RemoteEndPoint;
+            log.DebugFormat("{0}: received {1} from {2}", requestId, query, remoteEndPoint);
+            context.Request.InputStream.Close();
 
-			var hash = Convert.ToBase64String(CalcHash(Encoding.UTF8.GetBytes(query)));
-			var encryptedBytes = Encoding.UTF8.GetBytes(hash);
+            Thread.Sleep(delayMs);
 
-			context.Response.OutputStream.WriteAsync(encryptedBytes, 0, encryptedBytes.Length);
-			context.Response.OutputStream.Close();
-			log.InfoFormat("{0}: {1} sent back to {2}", requestId, hash, remoteEndPoint);
-		}
+            var hash = Convert.ToBase64String(CalcHash(Encoding.UTF8.GetBytes(query)));
+            var encryptedBytes = Encoding.UTF8.GetBytes(hash);
 
-		private static byte[] CalcHash(byte[] data)
-		{
-			using(var hasher = new HMACMD5(Key))
-				return hasher.ComputeHash(data);
-		}
+            context.Response.OutputStream.WriteAsync(encryptedBytes, 0, encryptedBytes.Length);
+            context.Response.OutputStream.Close();
+            log.DebugFormat("{0}: {1} sent back to {2}", requestId, hash, remoteEndPoint);
+        }
 
-		private const int port = 20000;
-		private static readonly byte[] Key = Encoding.UTF8.GetBytes("Контур.Шпора");
-		private static readonly ILog log = LogManager.GetLogger(typeof(Program));
-	}
+        private static byte[] CalcHash(byte[] data)
+        {
+            using (var hasher = new HMACMD5(Key))
+                return hasher.ComputeHash(data);
+        }
+
+        private static readonly byte[] Key = Encoding.UTF8.GetBytes("Контур.Шпора");
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+    }
 }
